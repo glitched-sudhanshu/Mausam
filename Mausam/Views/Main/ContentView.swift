4e28032d4365c38d3f4fb2daf9ac7fedf33b0933
glitched-sudhanshu <r02.sudhanshu@gmail.com>
2024-12-17 00:24:30 +0530

    [feat] home page

---
+++ Mausam/Views/Main/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Mausam
+//
+//  Created by Sudhanshu Ranjan on 16/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    HomeView()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ Mausam/Views/Main/HomeView.swift
@@ -0,0 +1,64 @@
+//
+//  HomeView.swift
+//  Mausam
+//
+//  Created by Sudhanshu Ranjan on 16/12/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+  var body: some View {
+    ZStack {
+      
+      //Mark: Background color
+      Color.background
+        .ignoresSafeArea()
+      
+      //Mark: Background Image
+      Image("Background")
+        .resizable()
+        .ignoresSafeArea()
+      
+      //Mark: House Image
+      Image("House")
+        .frame(maxHeight: .infinity, alignment: .top)
+        .padding(.top, 257)
+      
+      //Marl: Info
+      VStack(spacing: -10) {
+        Text("Montreal")
+          .font(.largeTitle)
+        
+        VStack {
+          Text(attributedString)
+          Text("H:24°   L:18°")
+            .font(.title3.weight(.semibold))
+        }
+        Spacer()
+      }.padding(.top, 51)
+    }
+  }
+  
+  private var attributedString: AttributedString {
+    var string = AttributedString("19°" + "\n " + "Mostly Clear")
+    if let temp = string.range(of: "19°") {
+      string[temp].font = .system(size: 96, weight: .thin)
+      string[temp].foregroundColor = .primary
+    }
+    if let pipe = string.range(of: " | ") {
+      string[pipe].font = .title3.weight(.semibold)
+      string[pipe].foregroundColor = .secondary
+    }
+    if let weather = string.range(of: "Mostly Clear") {
+      string[weather].font = .title3.weight(.semibold)
+      string[weather].foregroundColor = .secondary
+    }
+    return string
+  }
+}
+
+#Preview {
+  HomeView()
+    .preferredColorScheme(.dark)
+}
